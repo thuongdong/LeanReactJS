b9ab6b7d23c212c718940d38486ea24d526dce0d
thuongdd <dongducthuong@gmail.com>
2019-09-29 19:40:22 +0700

    add todo

--- todolist-reactjs/src/components/TaskForm.js
+++ todolist-reactjs/src/components/TaskForm.js
@@ -1,8 +1,38 @@
 import React from 'react';
 
 class TaskForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      status: false
+    }
+  }
   onCloseForm = () => {
     this.props.onCloseForm()
+  }
+  onChange = (event) => {
+    var target = event.target
+    var name = target.name
+    var value = target.value
+    if (name === 'status') {
+      value = target.value === 'true' ? true : false
+    }
+    this.setState({
+      [name]: value
+    })
+  }
+  onSubmit = (event) => {
+    event.preventDefault()
+    this.props.onSubmit(this.state)
+    this.onClear()
+    this.onCloseForm()
+  }
+  onClear = () => {
+    this.setState ({
+      name: '',
+      status: false
+    })
   }
   render() {
     return (
@@ -14,15 +44,24 @@ class TaskForm extends React.Component {
           </h3>
         </div>
         <div className="panel-body">
-          <form>
+          <form onSubmit={this.onSubmit}>
             <div className="form-group">
               <label>Ten: </label>
-              <input type="text" className="form-control" name="name" />
+              <input type="text" className="form-control" name="name" value={this.state.name} onChange={this.onChange}/>
             </div>
             <label>Trang thai: </label>
-            <select className="form-control" name="" id="">
-
-            </select>
+            <select className="form-control" name="status" value={this.state.status} onChange={this.onChange}>
+              <option value={true}>Kich hoat</option>
+              <option value={false}>An</option>
+            </select> <br />
+            <div className="text-center">
+              <button type="submit" className="btn btn-warning">
+                <span className="">Luu</span>
+              </button> &nbsp;
+              <button type="button" className="btn btn-danger" onClick={this.onClear}>
+                <span className="">Huy</span>
+              </button>
+            </div>
           </form>
         </div>
       </div>
